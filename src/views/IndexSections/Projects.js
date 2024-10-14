00b223e2d0b3788f9b970d4d7d691ca79ee49573
aihamnedal <94713844+aihamnedal@users.noreply.github.com>
2024-10-13 23:18:24 -0500

    Add 2024-2025 section to projects

--- src/views/IndexSections/Projects.js
+++ src/views/IndexSections/Projects.js
@@ -13,91 +13,91 @@ import {
   CardTitle,
 } from "reactstrap";
 
-export function Projects(props) {
+const ProjectCard = ({ img, projectTitle, projectDesc }) => {
   const imgHeight = 250;
+  return (
+    <Card>
+      <CardImg
+        src={img}
+        top
+        height={imgHeight}
+        style={{ objectFit: "cover" }}
+      />
+      <CardBody
+        className="bg-primary text-white"
+        style={{ borderRadius: "0 0 5px 5px" }}
+      >
+        <h3 className="text-white display-4">{projectTitle}</h3>
+        <small>
+          {projectDesc}
+        </small>
+      </CardBody>
+    </Card>
+  );
+}
+
+export function Projects(props) {
   return (
     <>
       <Row className="justify-content-center" id="projects">
         <h2>Our Projects</h2>
       </Row>
       <Container>
+        <h4>2024-2025</h4>
+        <hr className="my-3" />
+        <Row
+          className="px-3 justify-content-center justify-content-md-center current-projects"
+        >
+          <ProjectCard
+            img={require("assets/img/projects/coming_soon.webp")}
+            projectTitle="Solar E-Bike"
+            projectDesc=""
+          />
+          <ProjectCard
+            img={require("assets/img/projects/coming_soon.webp")}
+            projectTitle="GuitarAmp"
+            projectDesc=""
+          />
+          <ProjectCard
+            img={require("assets/img/projects/coming_soon.webp")}
+            projectTitle="CyberDeck"
+            projectDesc=""
+          />
+          <ProjectCard
+            img={require("assets/img/projects/coming_soon.webp")}
+            projectTitle="FM Radio"
+            projectDesc=""
+          />
+          <ProjectCard
+            img={require("assets/img/projects/coming_soon.webp")}
+            projectTitle="Parking Availability Software"
+            projectDesc=""
+          />
+        </Row>
+      </Container>
+      <Container>
         <h4>2023-2024</h4>
         <hr className="my-3" />
         <Row
           className="px-3 justify-content-center justify-content-md-center current-projects"
         >
-          <Card>
-            <CardImg
-              src={require("assets/img/projects/solar.webp")}
-              top
-              height={imgHeight}
-              style={{ objectFit: "cover" }}
-            />
-            <CardBody
-              className="bg-primary text-white"
-              style={{ borderRadius: "0 0 5px 5px" }}
-            >
-              <h3 className="text-white display-4">Solar Station</h3>
-              <small>
-                Solar powered charging station for phones and other mobile
-                devices
-              </small>
-            </CardBody>
-          </Card>
-          <Card>
-            <CardImg
-              src={require("assets/img/projects/metaldetector.webp")}
-              top
-              height={imgHeight}
-              style={{ objectFit: "cover" }}
-            />
-            <CardBody
-              className="bg-primary text-white w-max-50"
-              style={{ borderRadius: "0 0 5px 5px" }}
-            >
-              <h3 className="text-white display-4">Metal Detector</h3>
-              <small>
-                Small handheld metal detector using a coil to discover
-                conductive metals in its electromagnetic field
-              </small>
-            </CardBody>
-          </Card>
-          <Card>
-            <CardImg
-              src={require("assets/img/projects/boardgame.webp")}
-              top
-              height={imgHeight}
-              style={{ objectFit: "cover" }}
-            />
-            <CardBody
-              className="bg-primary text-white"
-              style={{ borderRadius: "0 0 5px 5px" }}
-            >
-              <h3 className="text-white display-4">Interactive Board Game</h3>
-              <small>
-                Small handheld metal detector using a coil to discover
-                conductive metals in its electromagnetic field
-              </small>
-            </CardBody>
-          </Card>
-          <Card>
-            <CardImg
-              src={require("assets/img/projects/speaker.webp")}
-              top
-              height={imgHeight}
-              style={{ objectFit: "cover" }}
-            />
-            <CardBody
-              className="bg-primary text-white"
-              style={{ borderRadius: "0 0 5px 5px" }}
-            >
-              <h3 className="text-white display-4">Bluetooth Speaker</h3>
-              <small>
-                Bluetooth speaker controlled by an Arduino with RGB LEDs and a
-                custom-built wood and plexiglass enclosure
-              </small>
-            </CardBody>
-          </Card>
+          <ProjectCard
+            img={require("assets/img/projects/solar.webp")}
+            projectTitle="Solar Station"
+            projectDesc="Solar powered charging station for phones and other mobile devices"
+          />
+          <ProjectCard
+            img={require("assets/img/projects/metaldetector.webp")}
+            projectTitle="Metal Detector"
+            projectDesc="Small handheld metal detector using a coil to discover
+                         conductive metals in its electromagnetic field"
+          />
+          <ProjectCard
+            img={require("assets/img/projects/speaker.webp")}
+            projectTitle="Bluetooth Speaker"
+            projectDesc="Bluetooth speaker controlled by an Arduino with RGB LEDs and a
+                         custom-built wood and plexiglass enclosure"
+          />
         </Row>
       </Container>
       <Container className="mt-5">
@@ -106,48 +106,21 @@ export function Projects(props) {
         <Row
           className="px-3 justify-content-center justify-content-md-center previous-projects"
         >
-          <Card>
-            <CardImg
-              src={require("assets/img/projects/drone.webp")}
-              top
-              height={imgHeight}
-              style={{ objectFit: "cover" }}
-            />
-            <CardBody
-              className="bg-primary text-white"
-              style={{ borderRadius: "0 0 5px 5px" }}
-            >
-              <h3 className="text-white display-4">RC Drone</h3>
-            </CardBody>
-          </Card>
-          <Card>
-            <CardImg
-              src={require("assets/img/projects/mirror.webp")}
-              top
-              height={imgHeight}
-              style={{ objectFit: "cover" }}
-            />
-            <CardBody
-              className="bg-primary text-white"
-              style={{ borderRadius: "0 0 5px 5px" }}
-            >
-              <h3 className="text-white display-4">Smart Mirror</h3>
-            </CardBody>
-          </Card>
-          <Card>
-            <CardImg
-              src={require("assets/img/projects/radio2.webp")}
-              top
-              height={imgHeight}
-              style={{ objectFit: "cover" }}
-            />
-            <CardBody
-              className="bg-primary text-white"
-              style={{ borderRadius: "0 0 5px 5px" }}
-            >
-              <h3 className="text-white display-4">FM Radio</h3>
-            </CardBody>
-          </Card>
+          <ProjectCard
+            img={require("assets/img/projects/drone.webp")}
+            projectTitle="RC Drone"
+            projectDesc=""
+          />
+          <ProjectCard
+            img={require("assets/img/projects/mirror.webp")}
+            projectTitle="Smart Mirror"
+            projectDesc=""
+          />
+          <ProjectCard
+            img={require("assets/img/projects/radio2.webp")}
+            projectTitle="FM Radio"
+            projectDesc=""
+          />
         </Row>
       </Container>
     </>
