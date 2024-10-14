dcf3407a9cfd4057ff42a63b999d5f3b59a066da
aihamnedal <94713844+aihamnedal@users.noreply.github.com>
2024-10-13 23:16:28 -0500

    Remove dead code (will need to move this back later)

--- src/views/IndexSections/ProjectLeads.js
+++ src/views/IndexSections/ProjectLeads.js
@@ -5,37 +5,6 @@ import { Col, Row } from "reactstrap";
 
 export function ProjectLeads(props) {
   return (
-    <Row
-      className="p-5 col-12 col-md-10"
-      style={{ margin: "0 auto" }}
-    >
-      <Col md className="pb-md-0 pb-3">
-        <Row className="justify-content-center">
-          <h2>Project Leads</h2>
-        </Row>
-        <Row className="headshot-list">
-          <Col sm>
-            <strong>Solar Station</strong>
-            <img src={require("assets/img/project-leads/lily.webp")} />
-            <p>Lily Bray</p>
-          </Col>
-          <Col sm>
-            <strong>Metal Detector</strong>
-            <img src={require("assets/img/project-leads/david.webp")} />
-            <p>David Levy</p>
-          </Col>
-          <Col sm>
-            <strong>Interactive Board Game</strong>
-            <img src={require("assets/img/project-leads/cristian.webp")} />
-            <p>Cristian Amaro</p>
-          </Col>
-          <Col sm>
-            <strong>Bluetooth Speaker</strong>
-            <img src={require("assets/img/project-leads/aiden.webp")} />
-            <p>Aiden Martinez</p>
-          </Col>
-        </Row>
-      </Col>
-    </Row>
+    <Row></Row>
   );
 }
